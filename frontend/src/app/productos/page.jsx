c37147db6fb50269c9b113de2a0e1c2568ce3a8c
SahirVl <114122177+SahirVl@users.noreply.github.com>
2025-09-26 07:16:11 -0500

    Update page.jsx

--- frontend/src/app/productos/page.jsx
+++ frontend/src/app/productos/page.jsx
@@ -3,7 +3,6 @@ import ProductList from "../components/ProductList"
 export default function ProductosPage() {
   return (
     <div className="p-6 max-w-6xl mx-auto">
-      <h1 className="text-2xl font-bold mb-4">Productos disponibles</h1>
       <ProductList />
     </div>
   );
